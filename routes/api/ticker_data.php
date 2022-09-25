a2296a502f8b98b691a8c0b213ded317b784e1e4
Dani Nedelchev <dngeorgiev@abv.bg>
2022-09-25 16:12:09 +0300

    refactor: Ran laravel pint to fix style issues

--- routes/api/ticker_data.php
+++ routes/api/ticker_data.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Api\TickerData\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::group([], function() {
+Route::group([], function () {
     Route::get('/{fromCurrency}/{toCurrency}/{chartType?}', IndexController::class)->name('index');
 });
